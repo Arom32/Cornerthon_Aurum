074ea810e12886e6e51256bc1ddb3f6d8e9d9953
Arom32 <jhhyun2005@naver.com>
2026-02-01 15:03:32 +0900

    feat : board/user model 수정, 일부 내용 추가
    1.  Board
    - 게시글 내용을 뜻하는description 을 content로 이름 변경
    - 거래 게시판 관련 주석 추가
    - countLikes 추가
    
    2. User
    - ViewCount 를 ReviewCount로 명칭 변경
    - 유저가 쓴 모든 게시글 개수인 totalPostCount 추가

--- backend/models/Board.js
+++ backend/models/Board.js
@@ -9,7 +9,7 @@ const boardSchema = new mongoose.Schema(
             enum: ["review", "question", "notice", "general", "trade"]
         },
         title: { type: String, required: true }, // 게시물 제목
-        description: { type: String, required: true }, // 게시물 내용
+        content: { type: String, required: true }, // 게시물 내용
         comments: [{ type: mongoose.Schema.Types.ObjectId }], // 댓글 배열
         // 작성자
         creator: {
@@ -17,19 +17,30 @@ const boardSchema = new mongoose.Schema(
             ref: 'User',
             required: true
         },
+
         // 후기 게시판용 (공연 연동)
         PerformId: { 
             type: mongoose.Schema.Types.ObjectId, 
             ref: "Performance" // 어떤 공연에 대한 리뷰인지
         },
-        price: { type: Number }, // 가격
+
+        // 거래 게시판용
+        price: { type: Number },  // 거래 가격
+
+        // 거래상태
         tradeStatus: {
             type: String,
             enum: ["available", "reserved", "completed"], // 거래 가능, 예약중, 완료
             default: "available"
         },
+
+        countLikes:{
+            type: Number,
+            default: 0
+        },
+
         // 추천한 유저들의 id 배열 (중복 방지용)
-        wholikes: [{ 
+        whoLikes: [{ 
             type: mongoose.Schema.Types.ObjectId, 
             ref: 'User' 
         }]
