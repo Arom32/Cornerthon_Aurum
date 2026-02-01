1ede75de538c3ece4290b3aec48c32d5c85d144f
“yeonu” <yeondu333@gmail.com>
2026-02-01 16:48:50 +0900

    feat: 회원가입/로그인 관련 API 구현

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,78 +1,26 @@
-/**
- * @swagger
- * tags:
- *   - name: Boards
- *     description: 게시물 관리 API
- */
+const express = require("express")
+const router = express.Router()
+const {getProfile, updateProfile, registerUser, loginUser, logoutUser} = require("../controllers/user")
+const protect = require("../middleWares/authMiddleware")
 
-/**
- * @swagger
- * /api/boards:
- *   post:
- *     summary: 게시물 작성
- *     tags:
- *       - Boards
- *     security:
- *       - bearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/BoardInput'
- *     responses:
- *       201:
- *         description: 작성 성공
- *       400:
- *         description: 필수 항목 누락
- */
-router.post('/', controller.createPost);
+// 회원가입  ( http://localhost:3000/api/user/register )
+router.route("/register")  
+.post(registerUser)
 
-/**
- * @swagger
- * /api/boards/{id}:
- *   patch:
- *     summary: 게시물 수정
- *     tags:
- *       - Boards
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     requestBody:
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/BoardUpdate'
- *     responses:
- *       200:
- *         description: 수정 성공
- */
-router.patch('/:id', controller.updatePost);
+// 로그인 ( http://localhost:3000/api/user/logout )
+router.route("/login")
+.get(loginUser)
 
-/**
- * @swagger
- * /api/boards/{id}:
- *   delete:
- *     summary: 게시물 삭제
- *     tags:
- *       - Boards
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: 삭제 성공
- */
-router.delete('/:id', controller.deletePost);
+// 로그아웃 ( http://localhost:3000/api/user/logout )
+router.route("/logout")
+.get(logoutUser)
 
-module.exports = router;
+// 프로필 조회 (칭호, 레벨, 포인트) ( http://localhost:3000/api/user/mypage )
+router.route("/mypage")
+.get(protect, getProfile)
+// 닉네임 수정
+.put(protect, updateProfile)
+
+
+
+module.exports = router
