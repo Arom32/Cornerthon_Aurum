84cfc499a3696131de450aaf9e4f3b02d0c0b4ae
chaewoni925 <popuri1216@naver.com>
2026-02-03 03:46:51 +0900

    fix: login 브랜치 내 불필요한 signup.jsx 삭제

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
-// src/App.jsx
 import { Routes, Route } from "react-router-dom";
-import SignupForm from "../signup/signup"; // 경로 확인!
+// App.jsx는 src 폴더에 있고, 컴포넌트들은 상위의 signup 폴더에 있으므로 ../ 를 써야 합니다.
+import LoginForm from "../login/login"; 
+import SignupForm from "../signup/signup"; 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SignupForm />} />
+      <Route path="/" element={<LoginForm />} />
+      <Route path="/login" element={<LoginForm />} />
       <Route path="/signup" element={<SignupForm />} />
     </Routes>
   );
 }
+
 export default App;
