7f727be9d9f95c576b64605392794fc75cf3444b
Arom32 <jhhyun2005@naver.com>
2026-02-02 13:15:48 +0900

    [BE] fix :  swagger에서 테스트 가능하도록 일부 파일 수정
    - Swagger 에서 정상 작동 되도록 미들웨어에 인증 방식 추가
    - board 라우터에 미들웨어 추가
    - 기존에 라우터에 존재하던 스웨거 주석 제거
    - user 라우터에 로그인 방식 get -> post로 수정

--- backend/routes/boardRoutes.js
+++ backend/routes/boardRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/board');
+const protect = require("../middleWares/authMiddleware")
+
+router.post('/', protect, controller.createPost);
 
 
-router.post('/', controller.createPost);
+router.patch('/:id', protect, controller.updatePost);
 
-
-router.patch('/:id', controller.updatePost);
-
-
-router.delete('/:id', controller.deletePost);
+router.delete('/:id', protect, controller.deletePost);
 
 module.exports = router;
