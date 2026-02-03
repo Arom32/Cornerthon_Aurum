e50f009fa6d1b4447d44435a6a6fcd0816900c1b
chaewoni925 <popuri1216@naver.com>
2026-02-04 04:14:25 +0900

    feat: 자유 게시판 업로드

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import LoginForm from "./login/login.jsx"; 
 import SignupForm from "./signup/signup.jsx"; 
+import Mypage from "./mypage/mypage.jsx";
+// 파일 최상단 import 문들 사이에 추가
+import { Link } from 'react-router-dom';
+import FreeBulletinBoard from './free-bulletin-board/free-bulletin-board.jsx';
 
 // 만약 원격(깃허브)에 다른 페이지가 있었다면 여기에 추가로 import 하세요.
 
@@ -10,6 +14,9 @@ function App() {
       <Route path="/" element={<LoginForm />} />
       <Route path="/login" element={<LoginForm />} />
       <Route path="/signup" element={<SignupForm />} />
+      <Route path="/mypage" element={<Mypage />} />
+      <Route path="/free-bulletin-board" element={<FreeBulletinBoard />} />
+      
       {/* 다른 팀원이 추가한 Route가 있다면 여기에 남겨두세요 */}
     </Routes>
   );
